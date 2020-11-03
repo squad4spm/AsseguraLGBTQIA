0aa14132ac12a2a41187cc185de7f75e534be727
KaiqueLimaNovaes <kaiquenovaes.lima@gmail.com>
2020-11-02 22:59:32 -0300

    Adição marcadores extras google maps

--- Projeto_Assegura/_js/maps.js
+++ Projeto_Assegura/_js/maps.js
@@ -2,7 +2,7 @@ function inicializar() {
     var coordenadas = {lat: -23.5677666, lng: -46.6487763};
 
     var mapa = new google.maps.Map(document.getElementById('mapa'), {
-      zoom: 15,
+      zoom: 14,
       center: coordenadas 
     });
 
@@ -11,4 +11,19 @@ function inicializar() {
         map: mapa,
         title: 'Meu marcador'
       });
+
+      /*Essa variavel cria novos marker com as coordenadas inseridas no array*/
+      var locations = [
+        ['Casa de acolhimento teste', -23.556402, -46.661947],
+      ];
+
+      var markerImage = 'marker.png';
+       
+      for (i = 0; i < locations.length; i++) {  
+        marker = new google.maps.Marker({
+          position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+          title: locations[i][0],
+          map: mapa,
+        });
+      }
 }
