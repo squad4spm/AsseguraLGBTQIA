b77154f84a3421247df14e51ae593c3c0f507a77
KaiqueLimaNovaes <kaiquenovaes.lima@gmail.com>
2020-11-05 00:53:57 -0300

    carrossel home

--- index.php
+++ index.php
@@ -38,51 +38,20 @@
             <li class="item">
                 <img src="./images/law.png" alt="">
                 <div class="descricaoG">
-                    <h2>Juridico</h2>
+                    <h2>Jurídico</h2>
                 </div>
             </li>
             <li class="item">
                 <img src="./images/psicologhy.png" alt="">
                 <div class="descricaoH">
-                    <h2>Pisicológico</h2>
+                    <h2>Psicológico</h2>
                 </div>
             </li>
         </ul>
     </div>
 
-    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-        <ol class="carousel-indicators">
-            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-        </ol>
-            <div class="carousel-inner">
-                <div class="carousel-item active">
-                    <div class="descricaoCardH">
-                        <h2>Pisicológico</h2>
-                    </div>
-                <div class="carousel-item">
-                    <div class="descricaoCardJ">
-                        <h2>Pisicológico</h2>
-                    </div>
-                </div>
-                <div class="carousel-item">
-                    <div class="descricaoCardH">
-                        <h2>Pisicológico</h2>
-                    </div>
-                </div>
-            </div>
-            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="sr-only">Previous</span>
-            </a>
-            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="sr-only">Next</span>
-            </a>
-        </div>
-    </div>
+    <?php include("carrosselHome.php"); ?>
 </main>
 </div>
   
-<?php // include('footer.php');?>
+<?php include('footer.php');?>
