ebf09a4528d1cd9c89924feda1d9672b2ef1bd9c
KaiqueLimaNovaes <kaiquenovaes.lima@gmail.com>
2020-11-05 00:02:19 -0300

    Att pg index

--- index.php
+++ index.php
@@ -1,90 +1,87 @@
 <?php include('header.php');?>
 <div id="antes_do_esquema_da_home">
 <main id="esquema_da_home">
-  <div class="d-none d-xl-block">
-  <div class="navbar">
-      <img src="./images/logoAssegura.png" alt="Logo assegura LGBTQIA+" width="210px" style="vertical-align:middle">
-      <ul class="iconsM">
-          <li class="item"><a href="defesaPessoal.php">
-              <img src="./images/selfDefense.png" alt="house icon"></a>
-              <div class="descricaoA">
-                  <h2>Defesa Pessoal</h2>
-              </div>
-          </li>
-          <li class="item"><a href="nomeSocial.php">
-              <img src="./images/doc.png" alt="law icon"></a>
-              <div class="descricaoJ">
-                  <h2>Nome social</h2>
-              </div>
-          </li>
-          <li class="item"><a href="localizacao.php">
-              <img src="./images/map.png" alt="icon doc"></a>
-              <div class="descricaoD">
-                  <h2>Localizações</h2>
-              </div>
-          </li>
-          <li class="item">
-              <img src="./images/shake.png" alt="icon">
-              <div class="descricaoE">
-                  <h2>Quer ajudar?</h2>
-              </div>
-          </li>
-          <li class="item">
-              <img src="./images/house.png" alt="">
-              <div class="descricaoF">
-                  <h2>Casa de acolhimento</h2>
-              </div>
-          </li>
-          <li class="item">
-              <img src="./images/law.png" alt="">
-              <div class="descricaoG">
-                  <h2>Juridico</h2>
-              </div>
-          </li>
-          <li class="item">
-              <img src="./images/psicologhy.png" alt="">
-              <div class="descricaoH">
-                  <h2>Pisicológico</h2>
-              </div>
-          </li>
-      </ul>
-  </div>
-  </div>
-  
-  <div class="d-none d-lg-block">
-        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-            <ol class="carousel-indicators">
+
+    <div class="navbar">
+        <img src="./images/logoAssegura.png" alt="Logo assegura LGBTQIA+" width="210px" style="vertical-align:middle">
+        <ul class="iconsM">
+            <li class="item"><a href="defesaPessoal.php">
+                <img src="./images/selfDefense.png" alt="house icon"></a>
+                <div class="descricaoA">
+                    <h2>Defesa Pessoal</h2>
+                </div>
+            </li>
+            <li class="item"><a href="nomeSocial.php">
+                <img src="./images/doc.png" alt="law icon"></a>
+                <div class="descricaoJ">
+                    <h2>Nome social</h2>
+                </div>
+            </li>
+            <li class="item"><a href="localizacao.php">
+                <img src="./images/map.png" alt="icon doc"></a>
+                <div class="descricaoD">
+                    <h2>Localizações</h2>
+                </div>
+            </li>
+            <li class="item">
+                <img src="./images/shake.png" alt="icon">
+                <div class="descricaoE">
+                    <h2>Quer ajudar?</h2>
+                </div>
+            </li>
+            <li class="item">
+                <img src="./images/house.png" alt="">
+                <div class="descricaoF">
+                    <h2>Casa de acolhimento</h2>
+                </div>
+            </li>
+            <li class="item">
+                <img src="./images/law.png" alt="">
+                <div class="descricaoG">
+                    <h2>Juridico</h2>
+                </div>
+            </li>
+            <li class="item">
+                <img src="./images/psicologhy.png" alt="">
+                <div class="descricaoH">
+                    <h2>Pisicológico</h2>
+                </div>
+            </li>
+        </ul>
+    </div>
+
+    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
             <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
             <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
             <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-            </ol>
+        </ol>
             <div class="carousel-inner">
-                    <div class="carousel-item active">
-                        <div class="descricaoCardH">
-                            <h2>Pisicológico</h2>
-                        </div>
-                    <div class="carousel-item">
-                        <div class="descricaoCardH">
-                            <h2>Pisicológico</h2>
-                        </div>
+                <div class="carousel-item active">
+                    <div class="descricaoCardH">
+                        <h2>Pisicológico</h2>
                     </div>
-                    <div class="carousel-item">
-                        <div class="descricaoCardH">
-                            <h2>Pisicológico</h2>
-                        </div>
+                <div class="carousel-item">
+                    <div class="descricaoCardJ">
+                        <h2>Pisicológico</h2>
+                    </div>
                 </div>
+                <div class="carousel-item">
+                    <div class="descricaoCardH">
+                        <h2>Pisicológico</h2>
+                    </div>
                 </div>
-                <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Previous</span>
-                </a>
-                <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Next</span>
-                </a>
             </div>
+            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
         </div>
-  </div>
+    </div>
 </main>
 </div>
   
