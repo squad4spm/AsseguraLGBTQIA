08f05629fc978967b45e6a7b90cadec0b4ded8f4
KaiqueLimaNovaes <kaiquenovaes.lima@gmail.com>
2020-11-06 00:00:51 -0300

    Att textos cards

--- carrosselHome.php
+++ carrosselHome.php
@@ -7,34 +7,78 @@
             <li data-target="#carouselExampleIndicators" data-slide-to="4"></li>
             <li data-target="#carouselExampleIndicators" data-slide-to="5"></li>
             <li data-target="#carouselExampleIndicators" data-slide-to="6"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="7"></li>
         </ol>
-            <div class="carousel-inner">
+        <div class="carousel-inner">
                 <div class="carousel-item active">
-                    <div class="descricaoCardA">
-                        <h2>Defesa pessoal</h2>
-                    </div>
+                    <a href="defesaPessoal.php">
+                        <div class="descricaoCardA">
+                            <img src="assets/images/logoAssegura-250px.png" alt="">
+                        </div>
+                    </a>
+            </div>
+
+            <div class="carousel-inner">
+                <div class="carousel-item">
+                    <a href="defesaPessoal.php">
+                        <div class="descricaoCardA">
+                            <h2>Defesa pessoal</h2>
+                            <p class="textoCards">
+                                Pesquisas mostram que uma pessoa treinada para responder adequadamente a um ataque, 
+                                pode até mesmo impedir que a agressão comece, pois, reagem ao perigo de forma mais assertiva e confiante, 
+                                aumentando as chances de evitar situações violentas ou enfrentá-las com segurança. 
+                            </p>
+                        </div>
+                    </a>
                 </div>
 
                 <div class="carousel-item">
-                    <div class="descricaoCardJ">
-                        <h2>Nome social</h2>
-                    </div>
+                    <a href="nomeSocial.php">
+                        <div class="descricaoCardJ">
+                            <h2>Nome social</h2>
+                            <p class="textoCards">
+                                Nomes estão diretamente ligados a um conceito de masculino e feminino 
+                                em nossa sociedade. Felizmente, o processo para que pessoas trans possam
+                                utilizar seu nome social em documentos oficiais ficou menos complicado e 
+                                mais inclusivo.
+                            </p>
+                        </div>
+                    </a>
                 </div>
 
                 <div class="carousel-item">
-                    <div class="descricaoCardD">
-                        <h2>Localizações</h2>
-                    </div>
+                    <a href="localizacao.php">
+                        <div class="descricaoCardD">
+                            <h2>Localizações</h2>
+                            <p class="textoCards">
+                                Conheça nosso mapa com a localização de 
+                                Escolas de defesa pessoal 
+                                Casas de acolhimento e Projetos sociais voltados para a comunidade 
+                                Lgbtqia+
+                                Centro de referência a Diversidade.
+                            </p>
+                        </div>
+                    </a>
                 </div>
                 
                 <div class="carousel-item">
                     <div class="descricaoCardE">
                         <h2>Quer ajudar?</h2>
+                        <a href="ajude.php">
+                            <p class="textoCards">
+                                Nós do ASSEGURA LGBTQIA+ acreditamos que sua ajuda é muito importante e significativa para todos. 
+                                Clique no botão saiba mais e conheça as formas que você pode contribuir e fazer parte de um mundo 
+                                cada vez melhor e inclusivo.
+                            </p>
+                        </a>
                     </div>
                 </div>
                 <div class="carousel-item">
                     <div class="descricaoCardF">
                         <h2>Casa de acolhimento</h2>
+                        <p class="textoCards">
+                            Conheça algumas Casas De Acolhimento Localizadas em São Paulo.
+                        </p>
                     </div>
                 </div>
                 <div class="carousel-item">
@@ -45,6 +89,11 @@
                 <div class="carousel-item">
                     <div class="descricaoCardH">
                         <h2>Psicológico</h2>
+                        <p class="textoCards">
+                            Cadastro Para Psicólogos Voluntários
+                            <br>
+                            Cadastro Para Conseguir Atendimento  
+                        </p>
                     </div>
                 </div>
             </div>
--- index.php
+++ index.php
@@ -5,46 +5,78 @@
     <div class="navbar">
         <img src="./images/logoAssegura.png" alt="Logo assegura LGBTQIA+" width="210px" style="vertical-align:middle">
         <ul class="iconsM">
-            <li class="item"><a href="defesaPessoal.php">
-                <img src="./images/selfDefense.png" alt="house icon"></a>
+            <li class="item">
+                <a href="defesaPessoal.php"><img src="./images/selfDefense.png" alt="house icon"></a>
                 <div class="descricaoA">
                     <h2>Defesa Pessoal</h2>
-                </div>
-            </li>
-            <li class="item"><a href="nomeSocial.php">
-                <img src="./images/doc.png" alt="law icon"></a>
-                <div class="descricaoJ">
-                    <h2>Nome social</h2>
-                </div>
-            </li>
-            <li class="item"><a href="localizacao.php">
-                <img src="./images/map.png" alt="icon doc"></a>
-                <div class="descricaoD">
-                    <h2>Localizações</h2>
+                    <p class="textoCards">
+                        Pesquisas mostram que uma pessoa treinada para responder adequadamente a um ataque, 
+                        pode até mesmo impedir que a agressão comece, pois, reagem ao perigo de forma mais assertiva e confiante, 
+                        aumentando as chances de evitar situações violentas ou enfrentá-las com segurança. 
+                    </p>
                 </div>
             </li>
             <li class="item">
-                <img src="./images/shake.png" alt="icon">
+                <a href="nomeSocial.php"><img src="./images/doc.png" alt="law icon"></a>
+                <div class="descricaoJ">
+                    <h2>Nome social</h2>
+                    <p class="textoCards">
+                        Nomes estão diretamente ligados a um conceito de masculino e feminino 
+                        em nossa sociedade. Felizmente, o processo para que pessoas trans possam
+                        utilizar seu nome social em documentos oficiais ficou menos complicado e 
+                        mais inclusivo.
+                    </p>
+                </div>
+            </li>
+            <li class="item">
+                <a href="localizacao.php"><img src="./images/map.png" alt="icon doc"></a>
+                <div class="descricaoD">
+                    <h2>Localizações</h2>
+                    <p class="textoCards">
+                        Conheça nosso mapa com a localização de 
+                        Escolas de defesa pessoal 
+                        Casas de acolhimento e Projetos sociais voltados para a comunidade 
+                        Lgbtqia+
+                        Centro de referência a Diversidade.
+                    </p>
+                </div>
+            </li>
+            <li class="item">
+                <a href="ajude.php"><img src="./images/shake.png" alt="icon"></a>
                 <div class="descricaoE">
                     <h2>Quer ajudar?</h2>
+                    <p class="textoCards">
+                        Nós do ASSEGURA LGBTQIA+ acreditamos que sua ajuda é muito importante e significativa para todos. 
+                        Clique no botão saiba mais e conheça as formas que você pode contribuir e fazer parte de um mundo 
+                        cada vez melhor e inclusivo.
+                    </p>
                 </div>
             </li>
             <li class="item">
                 <img src="./images/house.png" alt="">
                 <div class="descricaoF">
-                    <h2>Casa de acolhimento</h2>
+                    <h2>Casas de acolhimento</h2>
+                    <p class="textoCards">
+                        Conheça algumas Casas De Acolhimento Localizadas em São Paulo.
+                    </p>
                 </div>
             </li>
             <li class="item">
                 <img src="./images/law.png" alt="">
                 <div class="descricaoG">
-                    <h2>Jurídico</h2>
+                    <h2>J</h2>
+                    <p class="textoCards"></p>
                 </div>
             </li>
             <li class="item">
                 <img src="./images/psicologhy.png" alt="">
                 <div class="descricaoH">
                     <h2>Psicológico</h2>
+                    <p class="textoCards">
+                        Cadastro Para Psicólogos Voluntários
+                        <br>
+                        Cadastro Para Conseguir Atendimento  
+                    </p>
                 </div>
             </li>
         </ul>
